535f8cc676203bdee60f99fe345b83264b118fcf
IranNeto <iranmacedo.bneto@gmail.com>
2017-03-09 19:40:19 -0300

    to esp with divisor de tensão e map

--- sensor-current.ino
+++ sensor-current.ino
@@ -1,6 +1,4 @@
 #include <LiquidCrystal_I2C.h>
-
-//#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 //#include <LiquidCrystal_I2C.h>
 #include <dummy.h>
@@ -39,10 +37,11 @@ void setup() {
 void loop() {
 
   for (int i = 5000; i > 0; i--) {
+    int val = analogRead(pinoSensor);
+    sensorValue_aux = map(val, 10, 690, 0, 1023);
     // le o sensor na pino analogico A0 e ajusta o valor lido ja que a saída do sensor é (1023)vcc/2 para corrente =0
-    sensorValue_aux = (analogRead(pinoSensor) - 511);
     // somam os quadrados das leituras.
-    Serial.println((analogRead(pinoSensor)));
+    sensorValue_aux -= 511;
     valorSensor += sensorValue_aux * sensorValue_aux;
     delay(1);
   }
@@ -57,9 +56,9 @@ void loop() {
     }
   */
   // finaliza o calculo da média quadratica e ajusta o valor lido para volts
-  valorSensor = sqrt(abs(valorSensor) / 5000) * voltsporUnidade;
+  valorSensor = sqrt(valorSensor/5000)* voltsporUnidade;
   // calcula a corrente considerando a sensibilidade do sernsor (185 mV por amper)
-  valorCorrente = (valorSensor / sensibilidade);
+  valorCorrente = (valorSensor/sensibilidade);
 
   if (valorCorrente <= 0.095) {
     valorCorrente = 0;
@@ -68,6 +67,7 @@ void loop() {
 
   //Escreve
   //Mostra o valor da corrente
+  
   lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print("C(a): ");
@@ -76,9 +76,10 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print("P(w): ");
   lcd.setCursor(8, 1);
+  
   float pot = valorCorrente * tensao;
   lcd.print(pot);
-
+  
   Serial.print(valorCorrente);
   Serial.print("  ");
   Serial.println(pot);
